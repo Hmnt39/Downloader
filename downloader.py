c7d9fe1163baf288e2b9da5a49804bffa39090ad
Hmnt39 <hmnt39@gmail.com>
2019-05-28 21:00:07 +0530

    Modification

--- downloader.py
+++ downloader.py
@@ -1,7 +1,7 @@
 import bs4 as bs
 import urllib.request
 from pytube import YouTube
-import os
+import os, platform
 import time
 import math
 
@@ -15,16 +15,24 @@ def createFolder(directory):
 
 
 def response():
-    os.system("cls")
+    if platform.system() =='Windows':
+        os.system("cls")
+    else:
+        os.system("clear")
     print("WELCOME TO THE WEBCANVA YOUTUBE PLAYLIST DOWNLOADER \n Created by Webcanva (webcanva.com)")
     time.sleep(2)
+    print("Choose a downloading mode : \n 1. Single video \n 2. Playlist \n")
+    mode = int(input())
     url = input("Please paste the playlist link : ")
-    return url
+    return url, mode
 
 
 def thanks():
-    os.system("cls")
-    print("Thanks for using our software. If you have any suggestions please mail us \n (design.webcanva@gmail.com)" +
+    if platform.system() =='Windows':
+        os.system("cls")
+    else:
+        os.system("clear")
+    print("Thanks for using our software. If you have any suggestions please mail us \n (contact@webcanva.com)" +
           "\n Hemant Kumar Mishra")
     time.sleep(3)
 
@@ -32,7 +40,7 @@ def thanks():
 def playlist(url_link):
     url = urllib.request.urlopen(url_link)
     scrap = bs.BeautifulSoup(url, 'lxml')
-    videos = scrap.find_all("a", class_=" spf-link playlist-video clearfix yt-uix-sessionlink spf-link ")
+    videos = scrap.find_all("a", class_="spf-link playlist-video clearfix yt-uix-sessionlink spf-link")
     print(len(videos), " Videos found")
     video_link = []
     link = "https://www.youtube.com"
@@ -41,11 +49,7 @@ def playlist(url_link):
     return video_link
 
 
-def download():
-    url = response()
-##    directory = input("Type the folder name :")
-##    createFolder(directory)
-##    os.chdir(directory)
+def download(url):
     video_list = playlist(url)
     video_count = 0
     length = len(video_list)
@@ -61,7 +65,24 @@ def download():
         else:
             stream.download()
             print("Downloaded (" + str(video_count) + "/" + str(length) + ")")
+    #thanks()
+
+def single_video(url):
+    yt = YouTube(url)
+    stream = yt.streams.first()
+    print("Downloading " + yt.title + "(" + str(math.ceil(stream.filesize/(1024*1024))) + "MB)")
+    video_extension = stream.mime_type.split('/')
+    file_name = yt.title + "." + video_extension[1]
+    if os.path.exists(file_name):
+        print("File Exists")
+    else:
+        stream.download()
+        print(" Video Downloaded ")
     thanks()
 
-
-download()
+if __name__ =="__main__":
+    url, mode = response()
+    if mode == 1: 
+        single_video(url)
+    if mode == 2:
+        download(url)
